8cf8508cc24f16dcbdaa9301260c8923074d9c37
Molly Kent <kent.molly@gmail.com>
2015-04-17 16:44:37 -0700

    make initial card divs

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+'use strict';
+$(function(){
+  var table = $('#cardtable');
+  table.html('<div class="card"></div><div class="card"></div><div class="card"></div><div class="card"></div><div class="card"></div><div class="card"></div>');
+});
