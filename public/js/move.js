1a91dc7666892d3862b64ba509978bc168651928
Molly Kent <kent.molly@gmail.com>
2015-05-03 10:11:52 -0700

    drag and return invalid drops behavior

--- public/js/move.js
+++ public/js/move.js
@@ -1,10 +1,35 @@
 'use strict';
 $(function(){
-  var $moveable = $('#moveit')[0];
-  console.log($moveable);
+  $('#moveit1').draggable({revert:true});
+  $('#moveit2').draggable({revert:true});
+  $('.red').droppable({
+    accept: ".black",
+    tolerance: "intersect",
+    drop: function(ev, ui) {
+      console.log('dropped');
+      var dropped = ui.draggable;
+      var droppedOn = $(this);
+      $(dropped).detach();
+      $(dropped).css({top: 30,left: 0});
+      $(dropped).appendTo(droppedOn);
+    }
+  });
+  $('.black').droppable({
+    accept: ".red",
+    tolerance: "intersect",
+    drop: function(ev, ui) {
+      console.log('dropped');
+      var dropped = ui.draggable;
+      var droppedOn = $(this);
+      $(dropped).detach().css({top: 30,left: 0}).appendTo(droppedOn);
+    }
+  });
 
 
-    $("#moveit").draggable();
+  function cardSnap(droppedOn, droppedElement) {
+    $('#dropzone1').html('<div id="moveit1" class="red">' + $('#moveit1').html() + '</div>' );
+    $('#moveit1').remove();
+  }
 
 
 
